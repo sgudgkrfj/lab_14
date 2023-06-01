d62fe5c6c02dbc1bd5e852a5f3aa32c7b9bba62a
student <boyvegas955@gmail.com>
2023-06-01 20:48:00 +0300

    oop друге завдання

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def get_info(self):
+        print(f"ім'я студента: {self.name}, вік студента: {self.age}")
 
-
-
-
-
-
+st=Student("vasia", 12)
+st.get_info()
 
 
 
