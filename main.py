12f8a973aa730a7cc1813dabd01db2fed78cbb01
student <boyvegas955@gmail.com>
2023-06-01 20:57:17 +0300

    oop четверте завдання

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+class Animal:
+    def __init__(self, name, sound):
+        self.name=name
+        self.sound=sound
+    def soound(self):
+        print(f"Тварина: {self.name}, звук: {self.sound}")
 
-
-
-
-
-
-
-
+a=Animal("Kow", "moo")
+a.soound()
 
