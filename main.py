873522815960ebc6e3c9ca081788168d0f24458a
student <boyvegas955@gmail.com>
2023-06-01 20:44:10 +0300

    oop перше завдання

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+class BancAccount:
+    def __init__(self, account_number, balance):
+        self.account_number = account_number
+        self.balance = balance
 
+    def deposit(self, balan):
+        self.balance += balan
+        print(f"Додали гроші")
+    def withdraw(self, balan):
+        self.balance -= balan
+        print(f"Відняли гроші")
 
-
-
-
-
-
-
-
+banc = BancAccount(123, 1000)
+banc.deposit(10)
+print(banc.balance)
+banc.withdraw(100)
+print(banc.balance)
