b9dc45402b5eb118fd548f1c4a034bcdd8f23caa
student <boyvegas955@gmail.com>
2023-06-01 20:52:55 +0300

    oop третє завдання

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+class Book:
+    def __init__(self, title, author):
+        self.title=title
+        self.author=author
+    def get_info(self):
+        print(f"Назва книги: {self.title},автор: {self.author}")
 
-
-
-
-
-
+d=Book("hucdn", "I")
+d.get_info()
 
 
 
