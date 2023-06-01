a72023cc9b2a6b43545b4c4b6ca6f3664361428f
student <boyvegas955@gmail.com>
2023-06-01 19:41:59 +0300

    друге завдання

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-
-
-
-
-
+num_1=int(input())
+num_2=int(input())
+if num_1 > num_2:
+    s=0
+    for i in range(num_2, num_1+1):
+        s += i
+    print(s)
+elif num_2 > num_1:
+    s1 = 0
+    for i1 in range(num_1, num_2+1):
+        s1 += i1
+    print(s1)
 
 
 
